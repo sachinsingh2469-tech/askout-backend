ce9dae0b40090333271fe6ff28b3e0e52e81738d
sachinsingh2469-tech <sachinsingh2469@gmail.com>
2026-02-08 21:11:46 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://SachinMONGOdev:BwtVxm9PgtNGkhL@cluster0-shard-00-00.ysqos53.mongodb.net:27017,cluster0-shard-00-01.ysqos53.mongodb.net:27017,cluster0-shard-00-02.ysqos53.mongodb.net:27017/?ssl=true&replicaSet=atlas-xxxx-shard-0&authSource=admin&retryWrites=true&w=majority");
+
+const ResponseSchema = new mongoose.Schema({
+  answer: String,
+  time: String,
+  userAgent: String,
+});
+
+const Response = mongoose.model("Response", ResponseSchema);
+
+app.post("/yes", async (req, res) => {
+  const newResponse = new Response({
+    answer: "Yes",
+    time: new Date().toString(),
+    userAgent: req.headers["user-agent"],
+  });
+
+  await newResponse.save();
+  res.send({ status: "saved" });
+});
+
+app.listen(3000, () => console.log("Server running"));
